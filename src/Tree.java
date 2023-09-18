8d0a96a48c59aa5bc8b1d921ce415103467c5661
Alec Dong <alec.dong@mail.utoronto.ca>
2023-09-18 11:57:15 -0400

    tree class methods 7-11

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,44 @@
 public class Tree {
+    public double average() {
+        if (this.is_empty()) {
+            return 0.0;
+        } else {
+            int[] helper_results = this._average_helper();
+            return (double) helper_results[0] / helper_results[1];
+        }
+    }
+
+    private int[] _average_helper() {
+        if (this.is_empty()) {
+            return new int[] {0, 0};
+        } else {
+            int total = this._root;
+            int size = 1;
+            for (subtree : this._subtrees) {
+                int[] helper_results = subtree._average_helper();
+                total += helper_results[0];
+                size += helper_results[1];
+            }
+            return new int[] {total, size};
+        }
+    }
+
+    @Override
+    public boolean equals(Tree tree) {
+        if (this.is_empty() && tree.is_empty()) {
+            return true;
+        } else if (this.is_empty() || tree.is_empty()) {
+            return false;
+        } else {
+            if (this._root != tree._root) {
+                return false;
+            }
+
+            if (this._subtrees.size() != tree._subtrees.size()) {
+                return false;
+            }
+
+            return this._subtrees == tree._subtrees;
+        }
+    }
 }
